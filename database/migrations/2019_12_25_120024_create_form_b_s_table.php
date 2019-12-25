55b8d283d494a5c80802428115d73e04a2522f08
saitha vismith <saithavismith@gmail.com>
2019-12-25 18:48:01 +0530

    sasi update3

---
+++ database/migrations/2019_12_25_120024_create_form_b_s_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFormBSTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('form_b_s', function (Blueprint $table) {
+        
+            $table->bigIncrements('id');
+            $table->string('full_name');
+            $table->string('designation');
+            $table->string('department');
+            $table->string('faculty');
+            $table->string('tel');
+            $table->string('email');
+            $table->string('event');
+            $table->string('country');
+            $table->string('period');
+            $table->string('title');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('form_b_s');
+    }
+}
