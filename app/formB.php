6f155bc80a0f6d5ceea7e0f80cd2fe46babfbec0
saitha vismith <saithavismith@gmail.com>
2020-02-17 12:11:25 +0530

    update34

--- app/formB.php
+++ app/formB.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class formB extends Model
 {
-    //
+    protected $fillable=['full_name','designation','department','faculty','tel','email','event','country','period','title',];
 }
